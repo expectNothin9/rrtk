e64b9a5ad8e856584881b2635648d5f6f444b537
expectNothin9 <expect.nothin9@gmail.com>
2021-01-05 12:31:22 +0800

    fix: get rid of removed files dependency

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import projectReducer from '../features/project/projectSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     project: projectReducer
   }
 })
