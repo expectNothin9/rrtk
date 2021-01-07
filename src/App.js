5552bc639a59eb4aafa8d3be4c94dd8ac376cfb3
expectNothin9 <expect.nothin9@gmail.com>
2021-01-07 12:17:28 +0800

    feat: design-system.css, use css variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css'
 import Home from './pages'
 import Projects from './pages/projects'
+import NegativeEnergyMentor from './pages/negative-energy-mentor'
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projects} />
+          <Route exact path="/negative-engergy-mentor" component={NegativeEnergyMentor} />
         </Switch>
       </main>
     </Router>
